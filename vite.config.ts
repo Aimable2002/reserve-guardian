117983c62728580eb796e7377cb51ee071718485
Aimable Bimenyimana <153226014+Aimable2002@users.noreply.github.com>
2026-07-26 11:38:07 +0000

    modifed vite config ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,19 +14,8 @@ export default defineConfig({
       workbox: {
         globDirectory: ".output/public",
         globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
-        // Prevents Workbox from crashing if 0 files are found in .output/public prior to Nitro packaging
-        globStrict: false,
-        // Optional: fallback runtime cache rule so Workbox always has a valid config
-        runtimeCaching: [
-          {
-            urlPattern: ({ request }) => request.destination === "document",
-            handler: "NetworkFirst",
-            options: {
-              cacheName: "html-cache",
-            },
-          },
-        ],
-      },
+        globIgnores: ["**/node_modules/**/*"], // default anyway
+      }
     }),
   ],
 });
